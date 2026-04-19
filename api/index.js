20aba5e565404c9b7fbcd26029d26688325b5a27
J0hnAr1as <johnarias@tutamil.com>
2026-04-18 22:39:07 -0500

    se corrige archivo index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,5 @@
 const app = require("../app");
 
-export default function handler(req, res) {
-  const app = require("../app");
+module.exports = (req, res) => {
   return app(req, res);
-}
-module.exports = app;
+};
